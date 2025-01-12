7db2c5bfa2079cce8a0873a933327c79b960556f
Taleh Muzaffarov <talehmuzaffer@gmail.com>
2025-01-12 18:09:37 +0100

    social.sh

--- cron/social.sh
+++ cron/social.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Source the .env file to export variables into the current shell session
-source .env
-./venv/Scripts/activate
-# Now you can run your Python script or any other command with the environment variables loaded
-python cron/social.py "definition" "threads"
+# Check if arguments are passed
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <post_type> <soc>"
+  exit 1
+fi
+
+# Get the arguments
+post_type=$1
+soc=$2
+
+# Source the .env file from the parent directory
+if [ -f .env ]; then
+  source .env
+else
+  echo ".env file not found in the parent directory!"
+  exit 1
+fi
+
+# Activate virtual environment
+source ./venv/Scripts/activate  # Adjust the path if needed
+
+# Run your Python script with the arguments "post_type" and "soc"
+python cron/social.py "$post_type" "$soc"

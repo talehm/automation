3829c18ae480e38b34577784ff1b906f61d9aa27
Taleh Muzaffarov <talehmuzaffer@gmail.com>
2025-01-12 17:51:28 +0100

    bugfix

--- utils.py
+++ utils.py
@@ -7,7 +7,8 @@ import re
 
 def load_env_vars():
     # Load environment variables from .env file
-    load_dotenv()
+    dotenv_path = os.path.join(os.path.dirname(__file__), "", ".env")
+    load_dotenv(dotenv_path)
 
 
 def save_tokens_to_env(tokens):
